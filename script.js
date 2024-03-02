4ea8e574793444e9c6d58acc9d0416ede0e274fa
Nakul <nakulyadav1710@gmail.com>
2024-03-02 19:57:40 +0530

    javscript string

---
+++ script.js
@@ -0,0 +1,8 @@
+let str = 'abcd';
+console.log(str + 'efgh');
+console.log(str < 'efg');
+console.log("hello\nworld ");
+console.log('Result: ${10 + 10}')
+console.log('this os is\
+         amazing');
+console.log(str[3])
