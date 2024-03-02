a673071287bf4833ec0cbed0ce24e1045bc03648
Nakul <nakulyadav1710@gmail.com>
2024-03-02 20:14:03 +0530

    javscript string

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ console.log("hello\nworld ");
 console.log('Result: ${10 + 10}')
 console.log('this os is\
          amazing');
-console.log(str[3])
+console.log(str.charAt(1));
+console.log(str.includes('c'));
+console.log(str.startsWith('a'));
+console.log(str.endsWith('d'));
+console.log(str.toUpperCase().toLowerCase());
+console.log(str.substr(1));
+console.log(str.slice(1,3))
+console.log(str.padStart(6,'$'))
+console.log(str.padEnd(6,'$'))
+
+
