c92c83e49e3734ff4de86ecbf72841fc6041ce54
Nakul <nakulyadav1710@gmail.com>
2024-03-13 21:26:51 +0530

    Dom Manipulation

--- domManipulation.js
+++ domManipulation.js
@@ -14,4 +14,11 @@ const listItem = document.querySelectorAll('li');
 // listItems[0].style.color = 'red';
 // listItem[0].style.backgroundColor = 'blue';
 // listItem[0].value = 5;  // starts from 5 6 7 instead of  1 2 3
-listItem[0].setAttribute('value', '8');
+// listItem[0].setAttribute('class', ' big');
+// listItem[0].classList.remove('blue');
+listItem[0].classList.add('blue');
+listItem[0].classList.toggle('big');
+listItem[0].classList.toggle('big');
+listItem[0].classList.toggle('big');
+listItem[0].classList.toggle('big');//Alternately remove or add .
+
