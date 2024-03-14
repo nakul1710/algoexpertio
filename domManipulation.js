92e8e84a40a8cddb3e7ffcbfee9f954fe7100217
Nakul <nakulyadav1710@gmail.com>
2024-03-14 22:08:52 +0530

    Dom Manipulation

--- domManipulation.js
+++ domManipulation.js
@@ -3,22 +3,30 @@
 // const FirstLi = document.querySelector('Li');
 // console.log(secondLi);
 // console.log(FirstLi);
-const listItems = document.querySelectorAll('Li');
+// const listItems = document.querySelectorAll('li');
 // console.log(listItems);
 // Array.from(listItems).map(li => {
 //     console.log(li);
 // });
 // console.log(document.getElementByClassName('list-item'));
 // console.log(document.getElementByTagName('li'));
-const listItem = document.querySelectorAll('li');
+// const listItem = document.querySelectorAll('li');
 // listItems[0].style.color = 'red';
 // listItem[0].style.backgroundColor = 'blue';
 // listItem[0].value = 5;  // starts from 5 6 7 instead of  1 2 3
 // listItem[0].setAttribute('class', ' big');
 // listItem[0].classList.remove('blue');
-listItem[0].classList.add('blue');
-listItem[0].classList.toggle('big');
-listItem[0].classList.toggle('big');
-listItem[0].classList.toggle('big');
-listItem[0].classList.toggle('big');//Alternately remove or add .
+// listItem[0].classList.add('blue');
+// listItem[0].classList.toggle('big');
+// listItem[0].classList.toggle('big');
+// listItem[0].classList.toggle('big');
+// listItem[0].classList.toggle('big');//Alternately remove or add.
+const listItem = document.querySelectorAll('li');
+const text = document.createTextNode('Hello World');
+p.append(text);
+console.log(document.querySelector('ol').innerHTML);
+document.body.prepend(p);
+
+
+
 
